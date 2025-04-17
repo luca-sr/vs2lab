8b87976e3a7d3d2e7b1f486238b25fe4e3a49732
luca-sr <148057764+luca-sr@users.noreply.github.com>
2025-04-17 11:58:16 +0200

    lab1 complete i think

--- lab1/clientserver.py
+++ lab1/clientserver.py
@@ -8,60 +8,82 @@ import socket
 import const_cs
 from context import lab_logging
 
-lab_logging.setup(stream_level=logging.INFO)  # init loging channels for the lab
-
-# pylint: disable=logging-not-lazy, line-too-long
+# Initialize logging for the lab
+d = lab_logging.setup(stream_level=logging.INFO)
 
 class Server:
-    """ The server """
+    """The telephone directory server."""
     _logger = logging.getLogger("vs2lab.lab1.clientserver.Server")
     _serving = True
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # prevents errors due to "addresses in use"
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((const_cs.HOST, const_cs.PORT))
-        self.sock.settimeout(3)  # time out in order not to block forever
-        self._logger.info("Server bound to socket " + str(self.sock))
+        self.sock.settimeout(3)
+        self._logger.info(f"Server bound to socket {self.sock}")
+        # In-memory phonebook
+        self.phonebook = {
+            "Alice": "1234",
+            "Bob": "5678",
+            "Charlie": "9012"
+        }
 
     def serve(self):
-        """ Serve echo """
+        """Start serving incoming phonebook requests."""
         self.sock.listen(1)
-        while self._serving:  # as long as _serving (checked after connections or socket timeouts)
+        self._logger.info("Server listening for connections...")
+        while self._serving:
             try:
-                # pylint: disable=unused-variable
-                (connection, address) = self.sock.accept()  # returns new socket and address of client
-                while True:  # forever
-                    data = connection.recv(1024)  # receive data from client
+                conn, addr = self.sock.accept()
+                self._logger.info(f"Accepted connection from {addr}")
+                while True:
+                    data = conn.recv(1024)
                     if not data:
-                        break  # stop if client stopped
-                    connection.send(data + "*".encode('ascii'))  # return sent data plus an "*"
-                connection.close()  # close the connection
+                        break
+                    request = data.decode('utf-8').strip()
+                    self._logger.info(f"Received request: '{request}'")
+
+                    if request == "GETALL":
+                        response = str(self.phonebook)
+                    elif request.startswith("GET "):
+                        _, name = request.split(maxsplit=1)
+                        response = self.phonebook.get(name, "NOT FOUND")
+                    else:
+                        response = "INVALID REQUEST"
+
+                    self._logger.info(f"Sending response: '{response}'")
+                    conn.send(response.encode('utf-8'))
+
+                conn.close()
+                self._logger.info(f"Connection with {addr} closed")
             except socket.timeout:
-                pass  # ignore timeouts
+                pass
+
         self.sock.close()
-        self._logger.info("Server down.")
+        self._logger.info("Server shutdown.")
 
 
 class Client:
-    """ The client """
-    logger = logging.getLogger("vs2lab.a1_layers.clientserver.Client")
+    """The telephone directory client."""
+    _logger = logging.getLogger("vs2lab.lab1.clientserver.Client")
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((const_cs.HOST, const_cs.PORT))
-        self.logger.info("Client connected to socket " + str(self.sock))
+        self._logger.info(f"Client connected to socket {self.sock}")
 
-    def call(self, msg_in="Hello, world"):
-        """ Call server """
-        self.sock.send(msg_in.encode('ascii'))  # send encoded string as data
-        data = self.sock.recv(1024)  # receive the response
-        msg_out = data.decode('ascii')
-        print(msg_out)  # print the result
-        self.sock.close()  # close the connection
-        self.logger.info("Client down.")
-        return msg_out
+    def call(self, command="GET Alice"):
+        """Send a command to the server and return its response."""
+        self._logger.info(f"Sending command: '{command}'")
+        self.sock.send(command.encode('utf-8'))
+        data = self.sock.recv(4096)
+        response = data.decode('utf-8')
+        self._logger.info(f"Received response: '{response}'")
+        print(response)
+        return response
 
     def close(self):
-        """ Close socket """
+        """Close the client socket."""
         self.sock.close()
+        self._logger.info("Client socket closed.")
--- lab1/test_clientserver.py
+++ lab1/test_clientserver.py
@@ -1,43 +1,59 @@
-"""
-Simple client server unit test
-"""
-
 import logging
 import threading
 import unittest
+import time
 
 import clientserver
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
+logger = logging.getLogger("vs2lab.lab1.test_clientserver")
 
+class TestClientServer(unittest.TestCase):
+    """Test suite for client-server interaction"""
 
-class TestEchoService(unittest.TestCase):
-    """The test"""
-    _server = clientserver.Server()  # create single server in class variable
-    _server_thread = threading.Thread(target=_server.serve)  # define thread for running server
+    _server = clientserver.Server()
+    _server_thread = threading.Thread(target=_server.serve)
 
     @classmethod
     def setUpClass(cls):
-        cls._server_thread.start()  # start server loop in a thread (called only once)
+        logger.info("Setting up class: starting server thread...")
+        cls._server_thread.start()
+        time.sleep(1)  # Give server time to bind and start listening
 
     def setUp(self):
-        super().setUp()
-        self.client = clientserver.Client()  # create new client for each test
+        logger.info("Setting up test: creating new client...")
+        self.client = clientserver.Client()
 
-    def test_srv_get(self):  # each test_* function is a test
-        """Test simple call"""
-        msg = self.client.call("Hello VS2Lab")
-        self.assertEqual(msg, 'Hello VS2Lab*')
+    def test_get_existing_entry(self):
+        logger.info("Running test: GET existing entry (Alice)")
+        response = self.client.call("GET Alice")
+        logger.info(f"Response received: {response}")
+        self.assertEqual(response, "1234")
+
+    def test_getall_entries(self):
+        logger.info("Running test: GETALL all entries")
+        response = self.client.call("GETALL")
+        logger.info(f"Response received: {response}")
+        expected = str({"Alice": "1234", "Bob": "5678", "Charlie": "9012"})
+        self.assertEqual(response, expected)
+
+    def test_invalid_command(self):
+        logger.info("Running test: INVALID command")
+        response = self.client.call("HELLO")
+        logger.info(f"Response received: {response}")
+        self.assertEqual(response, "INVALID REQUEST")
 
     def tearDown(self):
-        self.client.close()  # terminate client after each test
+        logger.info("Tearing down test: closing client...")
+        self.client.close()
 
     @classmethod
     def tearDownClass(cls):
-        cls._server._serving = False  # break out of server loop. pylint: disable=protected-access
-        cls._server_thread.join()  # wait for server thread to terminate
-
+        logger.info("Tearing down class: stopping server...")
+        cls._server._serving = False
+        cls._server_thread.join()
+        logger.info("Server stopped.")
 
 if __name__ == '__main__':
     unittest.main()
