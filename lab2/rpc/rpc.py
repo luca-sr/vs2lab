9380ca3c9172ff1b42341cae29cba081cce9ff50
luca-sr <148057764+luca-sr@users.noreply.github.com>
2025-04-24 13:26:54 +0200

    a

--- lab2/rpc/rpc.py
+++ lab2/rpc/rpc.py
@@ -1,5 +1,6 @@
 import constRPC
-
+import threading
+import time
 from context import lab_channel
 
 
@@ -11,26 +12,42 @@ class DBList:
         self.value = self.value + [data]
         return self
 
-
 class Client:
     def __init__(self):
         self.chan = lab_channel.Channel()
         self.client = self.chan.join('client')
         self.server = None
+        self.running = True  # new_code
+        self.callback = None  # new_code
 
     def run(self):
         self.chan.bind(self.client)
         self.server = self.chan.subgroup('server')
+        threading.Thread(target=self.listen_for_results).start()  # new_code
 
     def stop(self):
+        self.running = False  # new_code
         self.chan.leave('client')
 
-    def append(self, data, db_list):
+    def append(self, data, db_list, callback):  # new_code
         assert isinstance(db_list, DBList)
-        msglst = (constRPC.APPEND, data, db_list)  # message payload
-        self.chan.send_to(self.server, msglst)  # send msg to server
-        msgrcv = self.chan.receive_from(self.server)  # wait for response
-        return msgrcv[1]  # pass it to caller
+        self.callback = callback  # new_code
+        msg = (constRPC.APPEND, data, db_list)  # new_code
+        self.chan.send_to(self.server, msg)  # new_code
+        ack = self.chan.receive_from(self.server)  # new_code
+        print("ACK received, continue working...")  # new_code
+
+    def listen_for_results(self):  # new_code
+        while self.running:  # new_code
+            msg = self.chan.receive_from_any(timeout=1)  # new_code
+            if msg and msg[1][0] == 'RESULT':  # new_code
+                _, result = msg[1]  # new_code
+                if self.callback:  # new_code
+                    self.callback(result)  # new_code
+                    self.callback = None  # new_code
+
+
+
 
 
 class Server:
@@ -41,18 +58,22 @@ class Server:
 
     @staticmethod
     def append(data, db_list):
-        assert isinstance(db_list, DBList)  # - Make sure we have a list
+        assert isinstance(db_list, DBList)
         return db_list.append(data)
 
     def run(self):
         self.chan.bind(self.server)
         while True:
-            msgreq = self.chan.receive_from_any(self.timeout)  # wait for any request
+            msgreq = self.chan.receive_from_any(self.timeout)
             if msgreq is not None:
-                client = msgreq[0]  # see who is the caller
-                msgrpc = msgreq[1]  # fetch call & parameters
-                if constRPC.APPEND == msgrpc[0]:  # check what is being requested
-                    result = self.append(msgrpc[1], msgrpc[2])  # do local call
-                    self.chan.send_to({client}, result)  # return response
-                else:
-                    pass  # unsupported request, simply ignore
+                client, msgrpc = msgreq
+                self.chan.send_to({client}, ('ACK',))  # new_code
+                threading.Thread(target=self.process_request, args=(client, msgrpc)).start()  # new_code
+
+    def process_request(self, client, msgrpc):  # new_code
+        time.sleep(10)  # new_code
+        if constRPC.APPEND == msgrpc[0]:  # new_code
+            result = self.append(msgrpc[1], msgrpc[2])  # new_code
+            self.chan.send_to({client}, ('RESULT', result))  # new_code
+
+
--- lab2/rpc/runcl.py
+++ lab2/rpc/runcl.py
@@ -1,16 +1,20 @@
 import rpc
 import logging
-
 from context import lab_logging
 
 lab_logging.setup(stream_level=logging.INFO)
+
+def handle_result(result):  # new_code
+    print("Callback: Result received:", result.value)  # new_code
 
 cl = rpc.Client()
 cl.run()
 
 base_list = rpc.DBList({'foo'})
-result_list = cl.append('bar', base_list)
+cl.append('bar', base_list, handle_result)  # new_code
 
-print("Result: {}".format(result_list.value))
+for i in range(5):  # new_code
+    print("Client doing other work...")  # new_code
+    import time; time.sleep(2)  # new_code
 
 cl.stop()
