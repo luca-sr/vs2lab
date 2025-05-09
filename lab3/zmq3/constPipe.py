323e41f60c209b25011e6bacfbaeef9a6404d279
luca-sr <148057764+luca-sr@users.noreply.github.com>
2025-05-09 11:35:49 +0200

    no comments

--- lab3/zmq3/constPipe.py
+++ lab3/zmq3/constPipe.py
@@ -3,10 +3,9 @@ SRC2 = "127.0.0.1"
 PORT1 = "50011"
 PORT2 = "50012"
 
-# constPipe.py
-SPLITTER_OUT_PORT = "6000"         # Splitter -> Mapper (PUSH)
-MAPPER1_IN_PORT = "6001"           # Mapper -> Reducer1
-MAPPER2_IN_PORT = "6002"           # Mapper -> Reducer2
+SPLITTER_OUT_PORT = "6000"         
+MAPPER1_IN_PORT = "6001"           
+MAPPER2_IN_PORT = "6002"
 
 SPLITTER_HOST = "127.0.0.1"
 REDUCER1_PORT = "7001"
